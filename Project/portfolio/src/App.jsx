1a581c07fbfe73a3cd62535dc880c6830ab5fb98
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-05-04 22:01:28 +0530

    started work on creating portfolio using React

---
+++ Project/portfolio/src/App.jsx
@@ -0,0 +1,5 @@
+import Navbar from "./components/Navbar"
+
+export default function navbar(){
+  return <Navbar></Navbar>
+}
---
+++ Project/portfolio/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import "../styles/Navbar.css"
+
+
+export default function Navbar(){
+  return (
+
+    <nav className="nav">
+        <h2 className="logo">Mehran</h2>
+
+        <ul className="links">
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+
+  );
+}
+
